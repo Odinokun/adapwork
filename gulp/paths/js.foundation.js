312ae118eb922a08d6c0c7e91e75a00814858cf0
odinokun <odinokun@gmail.com>
2023-10-31 07:58:51 +0100

    feat: create second section animation

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -4,6 +4,9 @@ module.exports = [
   './node_modules/jquery/dist/jquery.min.js',
   './node_modules/svg4everybody/dist/svg4everybody.min.js',
   './node_modules/swiper/swiper-bundle.js',
+  './node_modules/gsap/dist/gsap.js',
+  './node_modules/gsap/dist/ScrollTrigger.js',
+  // './node_modules/split-type/dist/index.js'
 
   // './src/vendors/jquery-ui/jquery-ui.min.js',
   // './src/vendors/maskedinput/jquery.maskedinput.min.js',
