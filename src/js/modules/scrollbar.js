c200cefcf63eebe7a56b51367a2dee1bee429a5e
odinokun <odinokun@gmail.com>
2023-11-02 19:30:25 +0100

    feat: inst section added

--- src/js/modules/scrollbar.js
+++ src/js/modules/scrollbar.js
@@ -1,5 +1,6 @@
 module.exports = function () {
 
+  // https://github.com/malihu/malihu-custom-scrollbar-plugin
   $(window).on("load", function () {
     $('.seo__container').mCustomScrollbar();
   });
